e623e4bb4816d54fcac766fb969e371c25465456
HaopengSun <sunhaopeng2016@gmail.com>
2021-06-12 21:55:56 -0400

    add full filled cells when the next round of particle insertion begins

--- algorithm/2D-algorithm.py
+++ algorithm/2D-algorithm.py
@@ -296,16 +296,62 @@ def poisson(r):
 		r = r - 10
 		poisson(r)
 
-def fill_the_void(roundOfInfilling, totalvolume, occupation, ideal_volume, roundRadius, rangeRadius, maximum):
-
-	gridnum = list(range(0, gridnumbers1))
-
+# omit cells which are fully covered by big spherical particles
+# they will not be checked by the next round of particle insertion
+def remove_cells(roundRadius):
 	for i in range(len(Circles)):
 		col_sub = math.floor(Circles[i].x / w1)
 		row_sub = math.floor(Circles[i].y / w1)
-		numb = col_sub + row_sub * cols1
-		grid1[numb] = 1
-		gridnum.remove(numb)
+		if Circles[i].r > w1:
+			range_circles = math.ceil((Circles[i].r + 2) / w1)
+			nums = list(range(-range_circles, range_circles + 1))
+			num1s = list(range(-range_circles, range_circles + 1))
+			for num in nums:
+				for num1 in num1s:
+					if 0 <= col_sub + num1 < cols1 and 0 <= row_sub + num < rows1:
+						index = (col_sub + num1) + (row_sub + num) * cols1
+						y_grid = math.floor(index / cols1)
+						x_grid = math.floor(index - y_grid * cols1)
+						y1 = y_grid * w1
+						x1 = x_grid * w1 
+						# distances of the eight corners of the cell and the particle center
+						d1 = dist(x1, y1, Circles[i].x, Circles[i].y)
+						d2 = dist((x1 + w1), y1, Circles[i].x, Circles[i].y)
+						d3 = dist(x1, (y1 + w1), Circles[i].x, Circles[i].y)
+						d4 = dist((x1 + w1), (y1 + w1), Circles[i].x, Circles[i].y)
+						distance_around = [d1, d2, d3, d4]
+						distance_around.sort()
+						distance_max = distance_around[2]
+						# if the maximum distance is smaller than the particle radius plus the minimun value of the radius range for the next round
+						# mark this cell as minus one
+						if distance_max <= Circles[i].r + roundRadius:
+							grid1[index] = -1
+		else:
+			# if the particle radius is smaller than the cell size, this cell will be marked as occupied directly
+			index = col_sub + row_sub * cols1
+			grid1[index] = -1
+	print("filter the non-filled cells")
+
+def fill_the_void(roundOfInfilling, totalvolume, occupation, ideal_volume, roundRadius, rangeRadius, maximum):
+
+	grid1 = list(range(0, gridnumbers1))
+
+	# for i in range(len(Circles)):
+	# 	col_sub = math.floor(Circles[i].x / w1)
+	# 	row_sub = math.floor(Circles[i].y / w1)
+	# 	numb = col_sub + row_sub * cols1
+	# 	grid1[numb] = 1
+	# 	gridnum.remove(numb)
+
+	remove_cells(roundRadius)
+
+	void_grid = []
+	non_filled_cells = 0
+	for i in range(gridnumbers1):
+		if grid1[i] != -1:
+			void_grid.append(grid1[i])
+			non_filled_cells = non_filled_cells + 1
+	gridnum = void_grid
 
 	# the loop will stop if the mass of this round of infilling reaches the target or it runs out of void grids
 	# the size of divided grids is 5, which can normally covers all round of infilling
@@ -537,3 +583,10 @@ else:
 # 2
 # 1, 0.76, 0.7, 0.47, 0.29, 0.18, 0.05, 0.02
 # void ratio = 0.6
+
+# unit = 0.025
+# width = 2400
+# height = 2400
+# sieves = 10, 4.75, 3.35, 2, 1, 0.85, 0.5, 0.3, 0.2, 0.1
+# soil_distributions = 1, 0.95, 0.93, 0.9, 0.68, 0.44, 0.2, 0.08, 0.02, 0.005
+# void ratio = 0.55
