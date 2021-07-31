813d0c271cd3f4dfbce13cdb674ed114b91741d8
serendipity-harpoon <45277284+serendipity-harpoon@users.noreply.github.com>
2021-07-31 18:37:33 -0400

    set up api in the djangp based backend

--- poisson/poisson/settings.py
+++ poisson/poisson/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
+    'rest_framework',
     'todo'
 ]
 
@@ -48,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'poisson.urls'
@@ -125,3 +128,9 @@ STATIC_URL = '/static/'
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'reactpoisson/build/static'),
 # ]
+
+# In the CORS_ORIGIN_WHITELIST code, you whitelisted localhost:3000 because you want the frontend
+# (which will be served on that port) of the application to interact with the API.
+CORS_ORIGIN_WHITELIST = [
+     'http://localhost:3000'
+]
--- poisson/todo/views.py
+++ poisson/todo/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from .serializers import TodoSerializer
+from .models import Todo
+
+# Create your views here.
+
+class TodoView(viewsets.ModelViewSet):
+    serializer_class = TodoSerializer
+    queryset = Todo.objects.all()
