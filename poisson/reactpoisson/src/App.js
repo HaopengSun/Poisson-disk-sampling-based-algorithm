092535f154282ff40c918b5cda3fc498b8d4efa2
serendipity-harpoon <45277284+serendipity-harpoon@users.noreply.github.com>
2021-07-31 20:30:44 -0400

    add react route and create Poisson component

--- poisson/reactpoisson/src/App.js
+++ poisson/reactpoisson/src/App.js
@@ -1,11 +1,30 @@
 import './App.css';
 import TodoList from './components/TodoList'
+import Poisson from './components/Poisson';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <TodoList />
-    </div>
+    <Router>
+      <div className="App">
+        <Link to="/todo">TodoList</Link>
+        <Link to="/">Poisson</Link>
+      </div>
+
+      <Switch>
+          <Route path="/todo">
+            <TodoList />
+          </Route>
+          <Route path="/">
+            <Poisson />
+          </Route>
+        </Switch>
+    </Router>
   );
 }
 
---
+++ poisson/reactpoisson/src/components/Poisson.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Poisson = function(){
+    return(
+        <div>Poisson Disk Sampling Algorithm</div>
+    )
+}
+
+export default Poisson
